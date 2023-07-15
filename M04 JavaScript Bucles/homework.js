a71d8412c89e40fdb19f928566bf3d354de2bc90
Tomas Romeo <83609306+Sekigan7@users.noreply.github.com>
2023-07-15 00:31:49 -0300

    Update homework.js

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -149,6 +149,18 @@ function operadoresLogicos(num1, num2, num3) {
    // Si todos los argumentos son cero, retornar ---> "Error".
    // Si no se cumple ninguna de las condiciones anteriores, retornar false.
    // Tu código:
+
+   if (num1 > num2 && num1 > num3 && num1 >= 1) {
+            return 'Numero 1 es mayor y positivo';
+      } else if (num1 < 0 || num2 < 0 || num3 < 0) { 
+            return 'Hay negativos';
+      } else if (num3 > num1 && num3 > num2) {
+            return num3 + 1;
+      } else if (num1 === 0 && num2 === 0 && num3 ===0) {
+         return 'Error';
+      } else {
+            return false;
+      }
 }
 
 function esPrimo(num) {
@@ -158,18 +170,38 @@ function esPrimo(num) {
    // [Pista 2]: puedes resolverlo utilizando un `bucle for`.
    // [Nota]: los números negativos, 0 y 1 NO son números primos.
    // Tu código:
+   if (num <2){
+      return false;
+   } else if (num === 2) {
+      return true
+   } for (var i = 2; i < num; i++){
+      if (num % i === 0){
+         return false;
+      }
+   }
+   return true
 }
 
 function esVerdadero(valor) {
    // Si "valor" es verdadero retornar "Soy verdadero".
    // Caso contrario, retornar "Soy falso".
    // Tu código:
+   if (valor === true){
+      return 'Soy verdadero';
+   }else{
+      return 'Soy falso';
+   }
 }
 
 function tieneTresDigitos(num) {
    // Si el número recibido tiene tres dígitos retornar true.
    // Caso contrario, retornar false.
    // Tu código:
+  if (num >= 100 && num <= 999){
+   return true;
+  } else{
+   return false;
+  }
 }
 
 function doWhile(num) {
@@ -177,6 +209,13 @@ function doWhile(num) {
    // Retornar el valor final.
    // Utilizar el bucle Do-While.
    // Tu código:
+   var a = 0
+   var i = num
+   do {
+      a = a + 1;
+      i = i + 5;
+   } while (a < 8);
+   return i;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
@@ -196,3 +235,4 @@ module.exports = {
    tieneTresDigitos,
    doWhile,
 };
+
